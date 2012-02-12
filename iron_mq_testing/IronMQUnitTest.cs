6d45c0097d053ab24a6535509c09c74af6c07b70
Corey Auger <coreyauger@gmail.com>
2012-02-12 13:06:59 -0800

    added Unit Test case.

---
+++ iron_mq_testing/IronMQUnitTest.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Configuration;
+
+using io.iron.ironmq; 
+using io.iron.ironmq.Data;
+
+namespace iron_mq_testing
+{
+  
+    /// <summary>
+    /// Summary description for IronMQUnitTest
+    /// </summary>
+    [TestClass]
+    public class IronMQUnitTest
+    {
+        private string _projectId = null;
+        private string _token = null;
+
+        public IronMQUnitTest()
+        {
+            _projectId = ConfigurationManager.AppSettings["IRONIO_PROJECT_ID"];
+            _token = ConfigurationManager.AppSettings["IRONIO_TOKEN"];
+            Assert.IsFalse( string.IsNullOrWhiteSpace(_projectId) );
+            Assert.IsFalse(string.IsNullOrWhiteSpace(_token));
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Client c = new Client(_projectId, _token);
+            Queue q = c.queue("test-queue");
+
+            string body = "Hello, IronMQ!";
+            q.push(body);
+
+            Message msg = q.get();
+            Assert.IsTrue( string.Compare( body, msg.Body) == 0 );
+            q.deleteMessage(msg);
+        }
+    }
+}
