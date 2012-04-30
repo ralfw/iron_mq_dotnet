76610452f42630be365ba3a34f6cbd4407a776e8
Oscar Deits <odeits@gmail.com>
2012-04-30 10:31:26 -0700

    Added support for bulk message pushing, included unit test of pushing 1k messages

--- iron_mq_dotnet/Queue.cs
+++ iron_mq_dotnet/Queue.cs
@@ -1,87 +1,112 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Script.Serialization;
-
 using io.iron.ironmq.Data;
 
 namespace io.iron.ironmq
 {
-    public class Queue
-    {
-        private Client client = null;
-        private string name = null;
-        private JavaScriptSerializer serializer = new JavaScriptSerializer();
+	public class Queue
+	{
+		private Client client = null;
+		private string name = null;
+		private JavaScriptSerializer serializer = new JavaScriptSerializer();
 
-        public Queue(Client client, string name)
-        {
-            this.client = client;
-            this.name = name;
-        }
+		public Queue(Client client, string name)
+		{
+			this.client = client;
+			this.name = name;
+		}
 
-        /**
-        * Retrieves a Message from the queue. If there are no items on the queue, an
-        * HTTPException is thrown.
-        *
-        * @throws HTTPException If the IronMQ service returns a status other than 200 OK.
-        * @throws IOException If there is an error accessing the IronMQ server.
-        */
-        public Message get()
-        {
-            string json = client.get("queues/" + name + "/messages");
-            QueueMessages queueResp = serializer.Deserialize<QueueMessages>(json);
-            return queueResp.messages.Length > 0 ? queueResp.messages[0] : null;
-        }
+		/**
+		* Retrieves a Message from the queue. If there are no items on the queue, an
+		* HTTPException is thrown.
+		*
+		* @throws HTTPException If the IronMQ service returns a status other than 200 OK.
+		* @throws IOException If there is an error accessing the IronMQ server.
+		*/
 
-        /**
-        * Deletes a Message from the queue.
-        *
-        * @param id The ID of the message to delete.
-        *
-        * @throws HTTPException If the IronMQ service returns a status other than 200 OK.
-        * @throws IOException If there is an error accessing the IronMQ server.
-        */
-        public void deleteMessage(String id)
-        {
-            client.delete("queues/" + name + "/messages/" + id);
-        }
+		public Message get()
+		{
+			string json = client.get("queues/" + name + "/messages");
+			QueueMessages queueResp = serializer.Deserialize<QueueMessages>(json);
+			return queueResp.messages.Length > 0 ? queueResp.messages[0] : null;
+		}
 
-        /**
-        * Deletes a Message from the queue.
-        *
-        * @param msg The message to delete.
-        *
-        * @throws HTTPException If the IronMQ service returns a status other than 200 OK.
-        * @throws IOException If there is an error accessing the IronMQ server.
-        */
-        public void deleteMessage(Message msg)
-        {
-            deleteMessage(msg.Id);
-        }
+		/**
+		* Deletes a Message from the queue.
+		*
+		* @param id The ID of the message to delete.
+		*
+		* @throws HTTPException If the IronMQ service returns a status other than 200 OK.
+		* @throws IOException If there is an error accessing the IronMQ server.
+		*/
 
-        /**
-        * Pushes a message onto the queue.
-        *
-        * @param msg The body of the message to push.
-        *
-        * @throws HTTPException If the IronMQ service returns a status other than 200 OK.
-        * @throws IOException If there is an error accessing the IronMQ server.
-        */
-        public void push(String msg)
-        {
-            push(msg, 0);
-        }
+		public void deleteMessage(String id)
+		{
+			client.delete("queues/" + name + "/messages/" + id);
+		}
 
-        /**
-        * Pushes a message onto the queue.
+		/**
+		* Deletes a Message from the queue.
+		*
+		* @param msg The message to delete.
+		*
+		* @throws HTTPException If the IronMQ service returns a status other than 200 OK.
+		* @throws IOException If there is an error accessing the IronMQ server.
+		*/
+
+		public void deleteMessage(Message msg)
+		{
+			deleteMessage(msg.Id);
+		}
+
+		/**
+		* Pushes a message onto the queue.
+		*
+		* @param msg The body of the message to push.
+		*
+		* @throws HTTPException If the IronMQ service returns a status other than 200 OK.
+		* @throws IOException If there is an error accessing the IronMQ server.
+		*/
+
+		public void push(String msg)
+		{
+			push(msg, 0);
+		}
+
+		/**
+		* Pushes a message onto the queue.
+		*
+		* @param msg The body of the message to push.
+		* @param timeout The timeout of the message to push.
+		*
+		* @throws HTTPException If the IronMQ service returns a status other than 200 OK.
+		* @throws IOException If there is an error accessing the IronMQ server.
+		*/
+
+		public void push(String msg, long timeout)
+		{
+			push(new string[] { msg }, timeout);
+		}
+
+		/**
+        * Pushes messages onto the queue.
         *
-        * @param msg The body of the message to push.
+        * @param msgs The body of the messages to push.
         * @param timeout The timeout of the message to push.
         *
         * @throws HTTPException If the IronMQ service returns a status other than 200 OK.
         * @throws IOException If there is an error accessing the IronMQ server.
         */
-        public void push(String msg, long timeout)
-        {
-            client.post("queues/" + name + "/messages", serializer.Serialize(new QueueMessages() { messages = new Message[] { new Message() { Body = msg, Timeout = timeout } } }));
-        }
-    }
+
+		public void push(IEnumerable<string> msgs, long timeout = 0)
+		{
+			client.post("queues/" + name + "/messages",
+				serializer.Serialize(new QueueMessages()
+				{
+					messages = msgs.Select(msg => new Message() { Body = msg, Timeout = timeout }).ToArray(),
+				}));
+		}
+	}
 }
--- iron_mq_testing/IronMQUnitTest.cs
+++ iron_mq_testing/IronMQUnitTest.cs
@@ -1,137 +1,161 @@
-﻿using System.Configuration;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
 using io.iron.ironmq;
 using io.iron.ironmq.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace iron_mq_testing
 {
-  
-    /// <summary>
-    /// Summary description for IronMQUnitTest
-    /// </summary>
-    [TestClass]
-    public class IronMQUnitTest
-    {
-        private string _projectId = null;
-        private string _token = null;
+	/// <summary>
+	/// Summary description for IronMQUnitTest
+	/// </summary>
+	[TestClass]
+	public class IronMQUnitTest
+	{
+		private string _projectId = null;
+		private string _token = null;
 
-        public IronMQUnitTest()
-        {
-            _projectId = ConfigurationManager.AppSettings["IRONIO_PROJECT_ID"];
-            _token = ConfigurationManager.AppSettings["IRONIO_TOKEN"];
-            Assert.IsFalse( string.IsNullOrWhiteSpace(_projectId) );
-            Assert.IsFalse(string.IsNullOrWhiteSpace(_token));
-        }
+		public IronMQUnitTest()
+		{
+			_projectId = ConfigurationManager.AppSettings["IRONIO_PROJECT_ID"];
+			_token = ConfigurationManager.AppSettings["IRONIO_TOKEN"];
+			Assert.IsFalse(string.IsNullOrWhiteSpace(_projectId));
+			Assert.IsFalse(string.IsNullOrWhiteSpace(_token));
+		}
 
-        private TestContext testContextInstance;
+		private TestContext testContextInstance;
 
-        /// <summary>
-        ///Gets or sets the test context which provides
-        ///information about and functionality for the current test run.
-        ///</summary>
-        public TestContext TestContext
-        {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
+		/// <summary>
+		///Gets or sets the test context which provides
+		///information about and functionality for the current test run.
+		///</summary>
+		public TestContext TestContext
+		{
+			get
+			{
+				return testContextInstance;
+			}
+			set
+			{
+				testContextInstance = value;
+			}
+		}
 
-        [TestMethod]
-        public void TestMethod1()
-        {
-            Client c = new Client(_projectId, _token);
-            Queue q = c.queue("test-queue");
-            ClearQueue(q);
+		[TestMethod]
+		public void TestMethod1()
+		{
+			Client c = new Client(_projectId, _token);
+			Queue q = c.queue("test-queue");
+			ClearQueue(q);
 
-            string body = "Hello, IronMQ!";
-            q.push(body);
+			string body = "Hello, IronMQ!";
+			q.push(body);
 
-            Message msg = q.get();
-            Assert.IsTrue( string.Compare( body, msg.Body) == 0 );
-            q.deleteMessage(msg);
-        }
+			Message msg = q.get();
+			Assert.IsTrue(string.Compare(body, msg.Body) == 0);
+			q.deleteMessage(msg);
+		}
 
-        private void ClearQueue(Queue q)
-        {
-            Message msg = null;
-            while ((msg  = q.get()) != null)
-            {
-                q.deleteMessage(msg.Id);
-            }
-        }
+		private void ClearQueue(Queue q)
+		{
+			Message msg = null;
+			while ((msg = q.get()) != null)
+			{
+				q.deleteMessage(msg.Id);
+			}
+		}
 
+		[TestMethod]
+		public void BasicTests()
+		{
+			Client c = new Client(_projectId, _token);
+			Queue q = c.queue("test-queue");
 
-        [TestMethod]
-        public void BasicTests()
-        {
-            Client c = new Client(_projectId, _token);
-            Queue q = c.queue("test-queue");
+			ClearQueue(q);
+			// clear_queue
+			q.push("hello world!");
+			Message msg = q.get();
+			Assert.IsNotNull(msg.Id);
+			Assert.IsNotNull(msg.Body);
+			//queue = @client.queues.get(:name=>@client.queue_name)
+			//total_messages = queue.total_messages
+			//res = @client.messages.post("hello world!")
+			//p res
+			//assert res["id"]
+			//assert res.id
+			//assert res.msg
 
-            ClearQueue(q);
-            // clear_queue            
-            q.push("hello world!");
-            Message msg = q.get();
-            Assert.IsNotNull( msg.Id );
-            Assert.IsNotNull( msg.Body );
-            //queue = @client.queues.get(:name=>@client.queue_name)
-            //total_messages = queue.total_messages
-            //res = @client.messages.post("hello world!")
-            //p res
-            //assert res["id"]
-            //assert res.id
-            //assert res.msg
+			//queue = @client.queues.get(:name=>@client.queue_name)
+			//assert queue.size == 1
+			//assert queue.total_messages == (total_messages+1)
 
-            //queue = @client.queues.get(:name=>@client.queue_name)
-            //assert queue.size == 1
-            //assert queue.total_messages == (total_messages+1)
-            
-           // msg = q.get();
-         //   Assert.IsNotNull( msg.Id );
-            //res = @client.messages.get()
-            //p res
-            //assert res["id"]
-            //assert res.id
+			// msg = q.get();
+			//   Assert.IsNotNull( msg.Id );
+			//res = @client.messages.get()
+			//p res
+			//assert res["id"]
+			//assert res.id
 
-            q.deleteMessage( msg.Id );
-            msg = q.get();
-            Assert.IsNull( msg );
-            //res = @client.messages.delete(res["id"])
-            //p res
-            //puts "shouldn't be any more"
-            //res = @client.messages.get()
-            //p res
-            //assert res.nil?
+			q.deleteMessage(msg.Id);
+			msg = q.get();
+			Assert.IsNull(msg);
+			//res = @client.messages.delete(res["id"])
+			//p res
+			//puts "shouldn't be any more"
+			//res = @client.messages.get()
+			//p res
+			//assert res.nil?
 
-            //queue = @client.queues.get(:name=>@client.queue_name)
-            //assert queue.size == 0
+			//queue = @client.queues.get(:name=>@client.queue_name)
+			//assert queue.size == 0
 
-            q.push("hello world 2!");
-            //res = @client.messages.post("hello world 2!")
-            //p res
-    
-            msg = q.get();
-            Assert.IsNotNull(msg);
-            //msg = @client.messages.get()
-            //p msg
-            //assert msg
+			q.push("hello world 2!");
+			//res = @client.messages.post("hello world 2!")
+			//p res
 
-            q.deleteMessage(msg.Id);
-            //res = msg.delete
-            //p res
+			msg = q.get();
+			Assert.IsNotNull(msg);
+			//msg = @client.messages.get()
+			//p msg
+			//assert msg
 
-            msg = q.get();
-            Assert.IsNull(msg);
-            //puts "shouldn't be any more"
-            //res = @client.messages.get()
-            //p res
-            //assert res.nil?
+			q.deleteMessage(msg.Id);
+			//res = msg.delete
+			//p res
 
-        }
-    }
+			msg = q.get();
+			Assert.IsNull(msg);
+			//puts "shouldn't be any more"
+			//res = @client.messages.get()
+			//p res
+			//assert res.nil?
+		}
+
+		/// <summary>
+		///A test for push
+		///</summary>
+		[TestMethod()]
+		public void bulkPushTest()
+		{
+			Client c = new Client(_projectId, _token);
+			Queue q = c.queue("test-queue");
+
+			ClearQueue(q);
+			var messages = Enumerable.Range(0, 1000).Select(i => i.ToString()).ToArray();
+			long timeout = 0; // TODO: Initialize to an appropriate value
+			q.push(messages, timeout);
+			var actual = new List<Message>(1000);
+			for (int i = 0; i < 1000; i++)
+			{
+				var msg = q.get();
+				Assert.IsNotNull(msg);
+				actual.Add(msg);
+			}
+			// Assumption is that if we queued up 1000 and we got back 1000 then it worked fine.
+			// Note: this does not verify we got back the same messages
+			foreach (var msg in actual)
+				q.deleteMessage(msg);
+		}
+	}
 }
