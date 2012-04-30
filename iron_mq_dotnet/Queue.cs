45c0a522b69c7794a82634e5b91dd4a9e846a89b
Oscar Deits <odeits@gmail.com>
2012-04-30 12:03:08 -0700

    Clear Queue implementation with unit test

--- iron_mq_dotnet/Queue.cs
+++ iron_mq_dotnet/Queue.cs
@@ -6,91 +6,102 @@ using io.iron.ironmq.Data;
 
 namespace io.iron.ironmq
 {
-	public class Queue
-	{
-		private Client client = null;
-		private string name = null;
-		private JavaScriptSerializer serializer = new JavaScriptSerializer();
+    public class Queue
+    {
+        private Client client = null;
+        private string name = null;
+        private JavaScriptSerializer serializer = new JavaScriptSerializer();
 
-		public Queue(Client client, string name)
-		{
-			this.client = client;
-			this.name = name;
-		}
+        public Queue(Client client, string name)
+        {
+            this.client = client;
+            this.name = name;
+        }
 
-		/**
-		* Retrieves a Message from the queue. If there are no items on the queue, an
-		* HTTPException is thrown.
-		*
-		* @throws HTTPException If the IronMQ service returns a status other than 200 OK.
-		* @throws IOException If there is an error accessing the IronMQ server.
-		*/
+        public void clear()
+        {
+            string emptyJsonObject = "{}";
+            var response = client.post("queues/" + name + "/clear", emptyJsonObject);
+            var responseObject = serializer.Deserialize<Dictionary<string, string>>(response);
+            if (responseObject["msg"] != "Cleared")
+            {
+                throw new Exception(string.Format("Unknown response from REST Endpoint : {0}", response));
+            }
+        }
 
-		public Message get()
-		{
-			string json = client.get("queues/" + name + "/messages");
-			QueueMessages queueResp = serializer.Deserialize<QueueMessages>(json);
-			return queueResp.messages.Length > 0 ? queueResp.messages[0] : null;
-		}
+        /**
+        * Retrieves a Message from the queue. If there are no items on the queue, an
+        * HTTPException is thrown.
+        *
+        * @throws HTTPException If the IronMQ service returns a status other than 200 OK.
+        * @throws IOException If there is an error accessing the IronMQ server.
+        */
 
-		/**
-		* Deletes a Message from the queue.
-		*
-		* @param id The ID of the message to delete.
-		*
-		* @throws HTTPException If the IronMQ service returns a status other than 200 OK.
-		* @throws IOException If there is an error accessing the IronMQ server.
-		*/
+        public Message get()
+        {
+            string json = client.get("queues/" + name + "/messages");
+            QueueMessages queueResp = serializer.Deserialize<QueueMessages>(json);
+            return queueResp.messages.Length > 0 ? queueResp.messages[0] : null;
+        }
 
-		public void deleteMessage(String id)
-		{
-			client.delete("queues/" + name + "/messages/" + id);
-		}
+        /**
+        * Deletes a Message from the queue.
+        *
+        * @param id The ID of the message to delete.
+        *
+        * @throws HTTPException If the IronMQ service returns a status other than 200 OK.
+        * @throws IOException If there is an error accessing the IronMQ server.
+        */
 
-		/**
-		* Deletes a Message from the queue.
-		*
-		* @param msg The message to delete.
-		*
-		* @throws HTTPException If the IronMQ service returns a status other than 200 OK.
-		* @throws IOException If there is an error accessing the IronMQ server.
-		*/
+        public void deleteMessage(String id)
+        {
+            client.delete("queues/" + name + "/messages/" + id);
+        }
 
-		public void deleteMessage(Message msg)
-		{
-			deleteMessage(msg.Id);
-		}
+        /**
+        * Deletes a Message from the queue.
+        *
+        * @param msg The message to delete.
+        *
+        * @throws HTTPException If the IronMQ service returns a status other than 200 OK.
+        * @throws IOException If there is an error accessing the IronMQ server.
+        */
 
-		/**
-		* Pushes a message onto the queue.
-		*
-		* @param msg The body of the message to push.
-		*
-		* @throws HTTPException If the IronMQ service returns a status other than 200 OK.
-		* @throws IOException If there is an error accessing the IronMQ server.
-		*/
+        public void deleteMessage(Message msg)
+        {
+            deleteMessage(msg.Id);
+        }
 
-		public void push(String msg)
-		{
-			push(msg, 0);
-		}
+        /**
+        * Pushes a message onto the queue.
+        *
+        * @param msg The body of the message to push.
+        *
+        * @throws HTTPException If the IronMQ service returns a status other than 200 OK.
+        * @throws IOException If there is an error accessing the IronMQ server.
+        */
 
-		/**
-		* Pushes a message onto the queue.
-		*
-		* @param msg The body of the message to push.
-		* @param timeout The timeout of the message to push.
-		*
-		* @throws HTTPException If the IronMQ service returns a status other than 200 OK.
-		* @throws IOException If there is an error accessing the IronMQ server.
-		*/
+        public void push(String msg)
+        {
+            push(msg, 0);
+        }
 
-		public void push(String msg, long timeout)
-		{
-			push(new string[] { msg }, timeout);
-		}
+        /**
+        * Pushes a message onto the queue.
+        *
+        * @param msg The body of the message to push.
+        * @param timeout The timeout of the message to push.
+        *
+        * @throws HTTPException If the IronMQ service returns a status other than 200 OK.
+        * @throws IOException If there is an error accessing the IronMQ server.
+        */
 
-		/**
+        public void push(String msg, long timeout)
+        {
+            push(new string[] { msg }, timeout);
+        }
+
+        /**
         * Pushes messages onto the queue.
         *
         * @param msgs The body of the messages to push.
@@ -100,13 +111,13 @@ namespace io.iron.ironmq
         * @throws IOException If there is an error accessing the IronMQ server.
         */
 
-		public void push(IEnumerable<string> msgs, long timeout = 0)
-		{
-			client.post("queues/" + name + "/messages",
-				serializer.Serialize(new QueueMessages()
-				{
-					messages = msgs.Select(msg => new Message() { Body = msg, Timeout = timeout }).ToArray(),
-				}));
-		}
-	}
+        public void push(IEnumerable<string> msgs, long timeout = 0)
+        {
+            client.post("queues/" + name + "/messages",
+                serializer.Serialize(new QueueMessages()
+                {
+                    messages = msgs.Select(msg => new Message() { Body = msg, Timeout = timeout }).ToArray(),
+                }));
+        }
+    }
 }
